c70a9baaebd32d47c397421e6e1ce44614192c0c
Lucas Verdade <lucasverdadegodoy@gmail.com>
2017-10-04 07:17:52 -0300

    subtraction code add

---
+++ sub.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+ 
+class sub
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+      System.out.println("Digite os dois numeros para subtrair: ");
+      Scanner in = new Scanner(System.in);
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x - y;
+      System.out.println("Subtracao dos numeros = "+z);
+   }
+}
